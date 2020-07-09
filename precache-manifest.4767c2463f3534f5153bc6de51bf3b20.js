1712037df605c5205e7e700312cbdcc13c0f03e9
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-07-09 14:06:21 +0200

    Updates

---
+++ precache-manifest.4767c2463f3534f5153bc6de51bf3b20.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7210025ca534e34df73510ca0af2832c",
+    "url": "/Portfolio-1/index.html"
+  },
+  {
+    "revision": "52356bfb520f977c9ff7",
+    "url": "/Portfolio-1/static/css/2.18bdc0c3.chunk.css"
+  },
+  {
+    "revision": "d0e1348b3bfa42d1a712",
+    "url": "/Portfolio-1/static/css/main.8daa21d4.chunk.css"
+  },
+  {
+    "revision": "52356bfb520f977c9ff7",
+    "url": "/Portfolio-1/static/js/2.73e230d1.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/Portfolio-1/static/js/2.73e230d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e1348b3bfa42d1a712",
+    "url": "/Portfolio-1/static/js/main.76166dab.chunk.js"
+  },
+  {
+    "revision": "a987e58a9a3ad47c1f90",
+    "url": "/Portfolio-1/static/js/runtime-main.16a16603.js"
+  },
+  {
+    "revision": "3f6202fe6382c9550736972c2a8f3efd",
+    "url": "/Portfolio-1/static/media/black.3f6202fe.jpg"
+  },
+  {
+    "revision": "b74e1eb143b2be5bcddc0dfaa4ced847",
+    "url": "/Portfolio-1/static/media/black2.b74e1eb1.jpg"
+  },
+  {
+    "revision": "db2a6e5c693cad67f889e271667c0472",
+    "url": "/Portfolio-1/static/media/black3.db2a6e5c.jpg"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/Portfolio-1/static/media/css.b848d5d9.png"
+  },
+  {
+    "revision": "25b694ba78065655dcee6237019aceda",
+    "url": "/Portfolio-1/static/media/html-5.25b694ba.png"
+  },
+  {
+    "revision": "d9de75e5bf3a7b0ef2a48321acc06e88",
+    "url": "/Portfolio-1/static/media/java.d9de75e5.png"
+  },
+  {
+    "revision": "07173b2b0d7deacf1bce262a1222fd96",
+    "url": "/Portfolio-1/static/media/mysql.07173b2b.png"
+  },
+  {
+    "revision": "533d8a274aaedeb2c17282ceba07c398",
+    "url": "/Portfolio-1/static/media/pic1.533d8a27.jpg"
+  },
+  {
+    "revision": "6c92d216cc16baac708b2022d2c61b10",
+    "url": "/Portfolio-1/static/media/pic2.6c92d216.jpg"
+  },
+  {
+    "revision": "8e11bf813d6b37d88b1867b376ffdacf",
+    "url": "/Portfolio-1/static/media/python.8e11bf81.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/Portfolio-1/static/media/react.f297dfcd.png"
+  }
+]);
